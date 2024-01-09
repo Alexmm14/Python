4a017a5c791e847cc97dda588b4d00f781c1661c
Alexmm14 <101550147+Alexmm14@users.noreply.github.com>
2024-01-09 14:27:43 -0600

    Update hola.py

--- hola.py
+++ hola.py
@@ -1,5 +1,7 @@
 import requests
 import json
+from docx import Document
+
 
 def get_first_contentful_paint(url, api_key):
     # Construir la URL de la API de PageSpeed Insights
@@ -20,13 +22,13 @@ def get_first_contentful_paint(url, api_key):
         print("------------------")
         return None
 
-def print_first_contentful_paint(url, api_key):
+"""def print_first_contentful_paint(url, api_key):
     fcp_value = get_first_contentful_paint(url, api_key)
 
     if fcp_value is not None:
         print("********************")
         print(f"First Contentful Paint (FCP): {fcp_value} segundos")
-        print("********************")
+        print("********************")"""
 
 def get_technology_info_formatted(url, api_key):
     api_url = 'https://whatcms.org/API/Tech'
@@ -35,28 +37,49 @@ def get_technology_info_formatted(url, api_key):
     response = requests.get(api_url, params=params)
     tech_info_raw = response.text
 
-    print("Entre en la funcion")
     try:
+
+        technologysData = []
+        technologys = {}
+        flag = 1
         hostname_json = json.loads(tech_info_raw)
         for result in hostname_json["results"]:
+            
             nombre = result["name"]
             version = result["version"] if "version" in result else "N/A"
             categorias = result["categories"] if "categories" in result else ["N/A"]
-
-            print("********************")
+            """print("********************")
             print(f"Nombre: {nombre}")
             print(f"Version: {version}")
-            print("Categorias:")
+            print("Categorias:")"""
+            technologysData.append(nombre)
+            if version:
+                technologysData.append(version)
+            else:
+                version = 'No se pudo obtener la version'
+                technologysData.append(version)
             
+            categories= []
             for categoria in categorias:
-                print(f" - {categoria}")
-                print("-" * 20)
+                """print(f" - {categoria}")
+                print("-" * 20)"""
+                categories.append(categoria)
+            technologysData.append(categories)
+            technologys['technology' + str(flag)] = technologysData
+            technologysData = []
+            flag += 1
+            
+            
+        return technologys
+    
 
     except json.JSONDecodeError as e:
         print(f"Error al decodificar JSON: {e}")
         print("\nRespuesta JSON (en bruto):")
         print("------------------")
         print(tech_info_raw)
+
+
 
 def getHostsThisSite(url, api_key):
     api_url = 'https://www.who-hosts-this.com/API/Host'
@@ -67,18 +90,46 @@ def getHostsThisSite(url, api_key):
 
     try:
         hostname_json = json.loads(tech_info_raw)
-        for result in hostname_json["results"]:
-            nombre = result["isp_name"]
 
-            print("********************")
+        for result in hostname_json.get("results", []):
+            nombre = result.get("isp_name", "N/A")
+            """print("********************")
             print(f"Nombre: {nombre}")
-            print("********************")
+            print("********************")"""
+            return nombre
 
     except json.JSONDecodeError as e:
         print(f"Error al decodificar JSON: {e}")
         print("\nRespuesta JSON (en bruto):")
         print("------------------")
         print(tech_info_raw)
+
+
+
+
+
+def crear_informe(diccionario):
+    herramientas = diccionario
+    
+    
+    # Crear un nuevo documento de Word
+    doc = Document()
+
+    # Añadir secciones al documento
+    doc.add_heading('Informe de Herramientas y Tiempo de Carga de Página Web', level=0)
+
+    # Introducción
+    doc.add_heading('1. Introducción:', level=2)
+    doc.add_paragraph('Breve descripción de la página web analizada.')
+
+    # Herramientas Utilizadas
+    doc.add_heading('2. Herramientas Utilizadas:', level=2)
+    for i in herramientas:
+        doc.add_paragraph(str(i))  # Asumiendo que los elementos de la lista son convertibles a cadenas
+
+    # Guardar el documento
+    doc.save('Informe_Herramientas_Tiempo_Carga.docx')
+    
 
 if __name__ == "__main__":
     # URL que deseas analizar
@@ -88,14 +139,18 @@ if __name__ == "__main__":
     api_key_psi = "AIzaSyAL67F5ZAG1eCWeoG5hAOwf2CYO_EfumAE"
 
     # Clave de API de WhatCMS
-    api_key_whatcms = "wm7tcnsm0tnautuld4kavikzryopxnesezlda3eim14ved490bfejc6s7xds5mkfk5bseo"
+    api_key_whatcms = "wq0ec3q7zwkhhko60d7d83856wy0ssvn1lnrbk7w01s327xtjn85up986vja0s70tjrd2l"
 
     # Obtener información de tecnología a través de la API en formato JSON formateado
-    get_technology_info_formatted(url, api_key_whatcms)
-    
-    
-    # Imprimir el valor de FIRST_CONTENTFUL_PAINT_MS
-    print_first_contentful_paint(url, api_key_psi)
+    datos = get_technology_info_formatted(url, api_key_whatcms)
 
-    getHostsThisSite(url, api_key_whatcms)
+    time = get_first_contentful_paint(url, api_key_psi)
 
+    host = getHostsThisSite(url, api_key_whatcms)
+
+
+    datos['host'] = host
+
+    datos['tiempo'] = str(time)
+
+    print(datos)
